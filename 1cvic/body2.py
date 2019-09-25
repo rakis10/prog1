cfc1741f5eca67874cb49452c8dfc8fb40200e7f
Juraj Rak <xrakj@cpu2.fei.stuba.sk>
2019-09-25 11:41:01 +0200

    hop

---
+++ 1cvic/body2.py
@@ -0,0 +1,30 @@
+import turtle
+import math
+
+
+a = 100;
+b = 100;
+
+for i in range(5):
+    #def kresli(i)
+    
+    turtle.forward(a)
+    turtle.left(90)
+    turtle.forward(b)
+    turtle.left(90)
+#print(math.radians(math.tan(1)))
+    turtle.goto(0,0)
+   # turtle.left(180)
+   #l = turtle.heading 
+    
+   # uhol = math.degrees( math.atan(b/a))
+   # uhol =  90 - uhol
+   # deg = uhol
+   # print(str(uhol) +" " +  str(a))# + "  " + a + " " + b )
+
+    #turtle.left(90 - uhol)
+#print(math.sqrt(100*100+100*100))
+   # c = math.sqrt(a**2+b**2)
+  #  turtle.forward(c)
+   # a = c
+   # turtle.left(180)
